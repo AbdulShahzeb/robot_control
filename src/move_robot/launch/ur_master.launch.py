eaf32644400fe944ed7f315608f9f94feb1a882a
Abdul Shahzeb <z5311131@ad.unsw.edu.au>
2025-07-29 15:28:32 +1000

    Changed launch params for Wk9 testing

--- src/move_robot/launch/ur_master.launch.py
+++ src/move_robot/launch/ur_master.launch.py
@@ -8,7 +8,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
 
     package_share_dir = get_package_share_directory('move_robot')
-    gcode_file = os.path.join(package_share_dir, 'gcode', 'six_cube.gcode')
+    gcode_file = os.path.join(package_share_dir, 'gcode', 'hemisphere.gcode')
 
     file_launch_arg = DeclareLaunchArgument(
         'file',
@@ -38,10 +38,10 @@ def generate_launch_description():
         name='gcode_interpreter',
         output='screen',
         parameters=[
-            {'x_offset': -920.0,
+            {'x_offset': -885.0,
             'y_offset': -250.0,
-            'z_offset': 193.8,
-            'print_speed_multiplier': 4.0,
+            'z_offset': 193.0,
+            'print_speed_multiplier': 3.0,
             'extrusion_scale_factor': 1.0,
             'wrist_angle': 90.0},
             {'file': LaunchConfiguration('file')}
